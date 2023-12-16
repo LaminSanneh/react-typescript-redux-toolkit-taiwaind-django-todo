05e8056f08137ebb944068dc072f3c93200dd76c
Lamin Sanneh <lamin.evra@gmail.com>
2023-12-16 00:19:02 +0000

    Style login and register components

--- todo_frontend/src/components/Register.tsx
+++ todo_frontend/src/components/Register.tsx
@@ -5,7 +5,62 @@ import { registerUser } from '../redux/actions/userActions';
 import { useNavigate } from 'react-router-dom';
 import { User } from '../redux/actions/types';
 
-const RegistrationForm: React.FC = () => {
+// const RegistrationForm: React.FC = () => {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const registeredUser = useAppSelector((state: RootState) => state.users);
+//   console.log("here", registeredUser);
+//   // console.log("here", registerUser.);
+//   const navigate = useNavigate();
+
+//   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setUsername(e.target.value);
+//   };
+
+//   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setPassword(e.target.value);
+//   };
+
+//   // useEffect(() => {
+//   //   if (registeredUser) {
+//   //     navigate('/login');
+//   //   }
+//   // }, [registeredUser, navigate]);
+
+//   const dispatch = useDispatch<AppDispatch>();
+
+//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     dispatch(registerUser({ username, password })).then(() => {
+//       navigate('/login');
+//     });
+//     // Handle form submission here (e.g., send data to backend)
+//     console.log('Username:', username);
+//     console.log('Password:', password);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Registration</h2>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label>Username:</label>
+//           <input type="text" value={username} onChange={handleUsernameChange} />
+//         </div>
+//         <div>
+//           <label>Password:</label>
+//           <input type="password" value={password} onChange={handlePasswordChange} />
+//         </div>
+//         <button type="submit">Register</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default RegistrationForm;
+
+export default function Example() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -35,27 +90,95 @@ const RegistrationForm: React.FC = () => {
     dispatch(registerUser({ username, password })).then(() => {
       navigate('/login');
     });
-    // Handle form submission here (e.g., send data to backend)
+    //     // Handle form submission here (e.g., send data to backend)
     console.log('Username:', username);
     console.log('Password:', password);
   };
-
   return (
-    <div>
-      <h2>Registration</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </div>
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
-};
+    <>
+      {/*
+        This example requires updating your template:
 
-export default RegistrationForm;
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full">
+        ```
+      */}
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  value={username}
+                  onChange={handleUsernameChange}
+                  id="email"
+                  name="email"
+                  type="text"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  value={password}
+                  onChange={handlePasswordChange}
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
--- todo_frontend/src/components/todos/List.tsx
+++ todo_frontend/src/components/todos/List.tsx
@@ -4,18 +4,38 @@ import { RootState, useAppSelector } from '../../redux/store';
 import { AppDispatch } from '../../redux/store';
 import { fetchTodos } from '../../redux/actions/todoActions';
 import { Todo } from '../../redux/actions/types';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import api from "../../api/session";
+import './List.css';
 
 const TodoList: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
   const todos = useAppSelector((state: RootState) => state.todos.todos);
-  const users = useAppSelector((state: RootState) => state.users.user);
+  const userState = useAppSelector((state: RootState) => state.users);
 
   console.log("here 2", todos)
-  console.log("here 3", users)
+  console.log("here 3", userState)
 
   useEffect(() => {
-    dispatch(fetchTodos());
+    // debugger;
+    // if (!userState.token) {
+    //   return null;
+    // }
+
+    // console.log('before dispatch fetch todos');
+    // console.log(api.defaults.headers);
+    // console.log(api.defaults.headers.Authorization);
+    dispatch(fetchTodos())
+      .then((e) => {
+        debugger
+
+        console.log("Fetched");
+        console.log(api.defaults.headers);
+        if (e?.meta?.requestStatus === "rejected") {
+          navigate('/login');
+        }
+      });
   }, [dispatch]);
 
   console.log("Here")
@@ -29,19 +49,50 @@ const TodoList: React.FC = () => {
     )
   } else {
     todosList = todos.map((todo: Todo) => {
-      return <li key={todo.id}>
-        <span>{todo.title}</span>
-        <span>{todo.description}</span>
-      </li>
+      // return (
+      //   <li key={todo.id}>
+      //     <span>{todo.title}</span>
+      //     <span>{todo.description}</span>
+      //   </li>
+      // )
+      return (
+        <li className="todo-item">
+          <p>hello</p>
+          <div className="todo-buttons">
+            <button className="todo-button" onClick={() => { }}>
+              Done
+            </button>
+            <button className="todo-button" onClick={() => { }}>
+              Delete
+            </button>
+          </div>
+        </li>
+      )
     });
   }
 
   return (
     <div>
       <Link to="/create">Add New Todo</Link>
-      <ul>
-        {todosList}
-      </ul>
+      <div className="todo-container">
+        <div className="todo-header">
+          <h1>Todo App</h1>
+        </div>
+        <form className="todo-form">
+          <input
+            type="text"
+            className="todo-input"
+            id="todoInput"
+            placeholder="Enter a new todo"
+          />
+          <div className="todo-buttons">
+            <button type="submit" className="todo-button">Add Todo</button>
+          </div>
+        </form>
+        <ul className="todo-list" id="todoList">
+          {todosList}
+        </ul>
+      </div>
     </div>
   );
 };
