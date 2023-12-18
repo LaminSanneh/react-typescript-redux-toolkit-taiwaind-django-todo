da15642374dc445fd0e7d208861dcd50d3f740c2
Lamin Sanneh <lamin.evra@gmail.com>
2023-12-18 10:30:56 +0000

    On login navigate user to the home page

--- todo_frontend/src/components/Login.tsx
+++ todo_frontend/src/components/Login.tsx
@@ -9,7 +9,7 @@ const LoginForm: React.FC = () => {
     const dispatch = useDispatch<AppDispatch>();
     const [credentials, setCredentials] = useState<User>({ username: '', password: '' });
     const navigate = useNavigate();
-    const loggedInUser = useAppSelector((state: RootState) => state.users);
+    const TOKEN_STORAGE_KEY = 'TOKEN_STORAGE_KEY';
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setCredentials({
@@ -26,47 +26,95 @@ const LoginForm: React.FC = () => {
             navigate('/');
         });
     };
+    useEffect(() => {
+        const token = localStorage.getItem(TOKEN_STORAGE_KEY);
+        if (token) {
+            dispatch(setUserToken(token)).then(() => {
+                navigate('/');
+            });
+        }
+    }, [dispatch, navigate])
+    // TODO: Move this out to another HOC so that only anonymus users can get to this page
+    // like protected route
 
-    // useEffect(() => {
-    //     debugger;
-    //     console.log('hey ya 1', loggedInUser, loggedInUser.token)
-    //     if (loggedInUser.token) {
-    //         dispatch(setUserToken(loggedInUser.token));
-    //         navigate('/');
-    //     }
-    //     // debugger
-    // }, [loggedInUser.token]);
-
-    // useEffect(() => {
-    //     debugger;
-    //     console.log('hey ya 1', loggedInUser, loggedInUser.token)
-    //     if (loggedInUser.token) {
-    //         dispatch(setUserToken(loggedInUser.token));
-    //         navigate('/');
-    //     }
-    //     // debugger
-    // }, [loggedInUser.token]);
-
-    debugger
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>
-                        Username:
-                        <input type="text" name="username" value={credentials.username} onChange={handleInputChange} />
-                    </label>
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        className="mx-auto h-10 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                    />
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Sign in to your account
+                    </h2>
                 </div>
-                <div>
-                    <label>
-                        Password:
-                        <input type="password" name="password" value={credentials.password} onChange={handleInputChange} />
-                    </label>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" onSubmit={handleLogin}>
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    value={credentials.username}
+                                    onChange={handleInputChange}
+                                    id="username"
+                                    name="username"
+                                    type="text"
+                                    autoComplete="email"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Password
+                                </label>
+                                <div className="text-sm">
+                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                        Forgot password?
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    value={credentials.password}
+                                    onChange={handleInputChange}
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Log In
+                            </button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Not a member?{' '}
+                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                            Start a 14 day free trial
+                        </a>
+                    </p>
                 </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+            </div>
+        </>
     );
 };
 
