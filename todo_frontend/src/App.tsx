bc552ab3bbcf0c7502758cdb108bbb140e050ad4
Lamin Sanneh <lamin.evra@gmail.com>
2023-12-18 10:33:31 +0000

    Did some cleanup and activated commented out routes

--- todo_frontend/src/App.tsx
+++ todo_frontend/src/App.tsx
@@ -1,77 +1,48 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useParams, useNavigate, useNavigation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TodoList from './components/todos/List';
 import UsersList from './components/users/List';
 import CreateTodoForm from './components/todos/Add';
 import EditTodoForm from './components/todos/Edit';
-import NotFound from './components/NotFound'; // Create a NotFound component if needed
+import NotFound from './components/NotFound';
 import RegistrationForm from './components/Register';
 import LogoutForm from './components/Logout';
 import LoginForm from './components/Login';
 import Navbar from './components/NavBar';
-import logo from './logo.svg';
-import './App.css';
-import { AppDispatch, useAppDispatch } from './redux/store';
+import { useAppDispatch } from './redux/store';
 import { setUserToken } from './redux/actions/userActions';
 import ProtectedRoute from './components/ProtectedRoute';
-
-const EditTodoRoute: React.FC = () => {
-  const { todoId } = useParams(); // Extract todoId from route params
-  return <EditTodoForm todoId={(Number)(todoId + '')} />;
-};
+import './App.css';
 
 const TOKEN_STORAGE_KEY = 'TOKEN_STORAGE_KEY';
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
-  // const navigate = useNavigate();
-  const initialTokenFromLocalStorage = localStorage.getItem(TOKEN_STORAGE_KEY);
 
-  // if (!initialTokenFromLocalStorage) {
-  //   debugger;
-  //   navigate('/login');
-  // }
-
-  // debugger;
-  useEffect(() => {
-    // console.log('hey ya 1', loggedInUser, loggedInUser.token)
-    // if (loggedInUser.token) {
-    //     navigate('/');
-    // }
-    // debugger
-
-    // const handleStorage = () => {
-    //   debugger;
-    //   const token = localStorage.getItem(TOKEN_STORAGE_KEY);
-
-    //   if (token) {
-    //     dispatch(setUserToken(token));
-    //   }
-    // }
-
-    // window.addEventListener('storage', handleStorage)
-    // return () => window.removeEventListener('storage', handleStorage)
-  }, []);
+  const token = localStorage.getItem(TOKEN_STORAGE_KEY);
+  if (token) {
+    dispatch(setUserToken(token));
+  }
 
   return (
     <BrowserRouter>
       <div>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<ProtectedRoute />}>
-            <Route path='/' element={<TodoList />} />
-          </Route> */}
-
           <Route path="/" element={
             <ProtectedRoute>
               <TodoList />
             </ProtectedRoute>
           } />
 
+          <Route path="/edit/:todoId" element={
+            <ProtectedRoute>
+              <EditTodoForm />
+            </ProtectedRoute>
+          } />
+
           <Route path="/users" element={<UsersList />} />
-          {/*<Route path="/create" element={<CreateTodoForm />} />
-          <Route path="/edit/:id" element={<EditTodoRoute />} />
-          <Route path="/register" element={<RegistrationForm />} /> */}
+          <Route path="/create" element={<CreateTodoForm />} />
+          <Route path="/register" element={<RegistrationForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/logout" element={<LogoutForm />} />
           <Route path="*" element={<NotFound />} />
